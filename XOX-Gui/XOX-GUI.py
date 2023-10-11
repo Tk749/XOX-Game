b1006ebc8f951a15a1bdcdf61ef52d9e2e6011c4
Tk749 <tahaknl.00@gmail.com>
2023-10-11 11:48:21 +0300

    UPDATE XOX-GUI.py

--- XOX-Gui/XOX-GUI.py
+++ XOX-Gui/XOX-GUI.py
@@ -1,90 +1,81 @@
-# importing Packages from tkinter
+# Importing necessary packages from tkinter
 from tkinter import *
 from tkinter import messagebox
 
+# Initializing player 1 as 'X' and a flag to stop the game
 Player1 = 'X'
 stop_game = False
 
-
+# Function to handle button click events with row and column arguments
 def clicked(r, c):
-    # player
     global Player1
-    # global stop_game
-
-    if Player1 == "X" and states[r] == 0 and stop_game == False:
-        b[r].configure(text="X")
-        states[r] = 'X'
+    # Check if it's player 1's turn and the cell is empty
+    if Player1 == "X" and states[r][c] == 0 and not stop_game:
+        # Set the button text to 'X' and update the state
+        b[r][c].configure(text="X")
+        states[r][c] = 'X'
+        # Switch to player 2's turn
         Player1 = 'O'
-
-    if Player1 == 'O' and states[r] == 0 and stop_game == False:
-        b[r].configure(text='O')
-        states[r] = "O"
+    # Check if it's player 2's turn and the cell is empty
+    elif Player1 == 'O' and states[r][c] == 0 and not stop_game:
+        # Set the button text to 'O' and update the state
+        b[r][c].configure(text='O')
+        states[r][c] = "O"
+        # Switch to player 1's turn
         Player1 = "X"
-
+    # Check if there's a winner after each move
     check_if_win()
-    # check_if_tie()
-    # if check_if_win() == False:
-    #     tie = messagebox.showinfo("tie","its tie")
-    #     return tie
 
-
+# Function to check if there's a winner
 def check_if_win():
     global stop_game
-    # count = 0
 
+    # Check for horizontal wins
     for i in range(3):
         if states[i][0] == states[i][1] == states[i][2] != 0:
             stop_game = True
-
-            winner = messagebox.showinfo("Winner", states[i][0] + " Won")
-            # disableAllButton()
+            # Show a message box indicating the winner
+            winner = messagebox.showinfo("Winner", states[i][0] + " Won!")
             break
 
-        # for j in range(3):
-        elif states[0][i] == states[1][i] == states[2][i] != 0:
+    # Check for vertical wins
+    for i in range(3):
+        if states[0][i] == states[1][i] == states[2][i] != 0:
             stop_game = True
-
             winner = messagebox.showinfo("Winner", states[0][i] + " Won!")
             break
 
-        elif states[0][0] == states[1][1] == states[2][2] != 0:
-            stop_game = True
+    # Check for diagonal wins
+    if states[0][0] == states[1][1] == states[2][2] != 0:
+        stop_game = True
+        winner = messagebox.showinfo("Winner", states[0][0] + " Won!")
 
-            winner = messagebox.showinfo("Winner", states[0][0] + " Won!")
-            break
+    if states[0][2] == states[1][1] == states[2][0] != 0:
+        stop_game = True
+        winner = messagebox.showinfo("Winner", states[0][2] + " Won!")
 
-        elif states[0][2] == states[1][1] == states[2][0] != 0:
-            stop_game = True
+    # Check for a tie (if all cells are filled)
+    if all(states[i][j] != 0 for i in range(3) for j in range(3)) and not stop_game:
+        stop_game = True
+        tie = messagebox.showinfo("Tie", "It's a Tie!")
 
-            winner = messagebox.showinfo("Winner", states[0][2] + " Won!")
-            break
-
-        elif states[0][0] and states[0][1] and states[0][2] and states[1][0] and states[1][1] and states[1][2] and \
-                states[2][0] and states[2][1] and states[2][2] != 0:
-            stop_game = True
-
-            winner = messagebox.showinfo("tie", "Tie")
-
-
-# Design window
-# Creating the Canvas
+# Design the window
 root = Tk()
-# Title of the window            
-root.title("GeeksForGeeks-:Tic Tac Toe")
+root.title("Tic Tac Toe")
 root.resizable(0, 0)
 
-# Button
+# Create buttons and states for the game board
 b = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]]
 
-# text for buttons
 states = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]]
 
+# Create and configure buttons for the game board
 for i in range(3):
     for j in range(3):
         b[i][j] = Button(
@@ -93,4 +84,5 @@ for i in range(3):
             command=lambda r=i, c=j: clicked(r, c))
         b[i][j].grid(row=i, column=j)
 
+# Start the main loop
 mainloop()
